0567ca415edf20210abde80d6268b5dae1bc8024
BlazerR <br229@njit.edu>
2017-02-26 20:50:13 -0500

    10

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -1,50 +1,19 @@
-<?php
-require_once('database.php');
-
-// Get all categories
-$query = 'SELECT * FROM categories
-                       ORDER BY categoryID';
-$statement = $db->prepare($query);
-$statement->execute();
-$categories = $statement->fetchAll();
-$statement->closeCursor();
-?>
-<!DOCTYPE html>
-<html>
-
-<!-- the head section -->
-<head>
-    <title>My Guitar Shop</title>
-    <link rel="stylesheet" type="text/css" href="main.css" />
-</head>
-
-<!-- the body section -->
-<body>
-<header><h1>Product Manager</h1></header>
+<?php include '../view/header.php'; ?>
 <main>
+
     <h1>Category List</h1>
     <table>
         <tr>
             <th>Name</th>
             <th>&nbsp;</th>
-        </tr>
-        
-        <!-- add code for the rest of the table here -->
-    
+        </tr>        
+        <!-- add category rows here -->
     </table>
 
     <h2>Add Category</h2>
-    
-    <!-- add code for the form here -->
-    
-    <br>
-    <p><a href="index.php">List Products</a></p>
+    <!-- add code for form here -->
 
-    </main>
+    <p><a href="index.php?action=list_products">List Products</a></p>
 
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
-    </footer>
-</body>
-</html>
-?>
+</main>
+<?php include '../view/footer.php'; ?>
